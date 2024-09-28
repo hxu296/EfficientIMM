c548b343931dd36f965418ccb1a36d070b6d551b
Xu <hxu398@krishna-srv1.ece.gatech.edu>
2024-09-28 01:40:15 -0400

    Auto configure best numa setting for exp

--- run_strong_scaling_ic_eimm.sh
+++ run_strong_scaling_ic_eimm.sh
@@ -14,7 +14,9 @@ datasets=(
 # Get number of total cores (excluding hyperthreading)
 phys_cores=$(lscpu | grep "^Core(s) per socket:" | awk '{print $4}')
 sockets=$(lscpu | grep "^Socket(s):" | awk '{print $2}')
+numas=$(lscpu | grep "^NUMA node(s):" | awk '{print $3}')
 total_phys_cores=$((phys_cores * sockets))
+core_per_numa=$((total_phys_cores / numas))
 threads=()
 
 # Double the value until the total number of physical cores is reached
@@ -33,32 +35,14 @@ mkdir -p "$log_dir"
 
 for dataset in "${datasets[@]}"; do
   for thread in "${threads[@]}"; do
+    if [ $thread -gt 128 ]; then
+      continue
+    fi
     dataset_name=$(basename "$dataset" .tsv)
     log_file="${log_dir}/${dataset_name}_threads_${thread}.json"
-
-    case $thread in
-      128)
-        numa_cmd="numactl -a -i -C 0-127"
-        ;;
-      64)
-        numa_cmd="numactl -a -i all -C 0-63"
-        ;;
-      32)
-        numa_cmd="numactl -a -i all -C 0-31"
-        ;;
-      16)
-        numa_cmd="numactl -a -i all -C 0-15"
-        ;;
-      8)
-        numa_cmd="numactl -a -i all -C 0-7"
-        ;;
-      4)
-        numa_cmd="numactl -a -i all -C 0-3"
-        ;;  
-      2)
-        numa_cmd="numactl -a -i all -C 0-1"
-        ;;        
-    esac
+    numa_node_param=0-$(( (thread / core_per_numa) - ( (thread % core_per_numa == 0) ? 1 : 0 ) ))
+    core_binding_param=0-$((thread - 1))
+    numa_cmd="numactl -a -i ${numa_node_param} -C ${core_binding_param}"
 
     if [[ "$dataset" == *"ungraph"* ]]; then
       OMP_PLACES=cores OMP_NUM_THREADS=$thread $numa_cmd $imm_bin -i "${test_data_dir}/$dataset" -u -w -p -k 50 -d IC -e 0.5 -o "$log_file"
--- run_strong_scaling_lt_ripples.sh
+++ run_strong_scaling_lt_ripples.sh
@@ -14,7 +14,9 @@ datasets=(
 # Get number of total cores (excluding hyperthreading)
 phys_cores=$(lscpu | grep "^Core(s) per socket:" | awk '{print $4}')
 sockets=$(lscpu | grep "^Socket(s):" | awk '{print $2}')
+numas=$(lscpu | grep "^NUMA node(s):" | awk '{print $3}')
 total_phys_cores=$((phys_cores * sockets))
+core_per_numa=$((total_phys_cores / numas))
 threads=()
 
 # Double the value until the total number of physical cores is reached
@@ -33,23 +35,14 @@ mkdir -p "$log_dir"
 
 for dataset in "${datasets[@]}"; do
   for thread in "${threads[@]}"; do
+    if [ $thread -gt 128 ]; then
+      continue
+    fi
     dataset_name=$(basename "$dataset" .tsv)
     log_file="${log_dir}/${dataset_name}_threads_${thread}.json"
-
-    case $thread in
-      128)
-        numa_cmd="numactl -a -i all -C 0-127"
-        ;;
-      64)
-        numa_cmd="numactl -a -i all -C 0-63"
-        ;;
-      32)
-        numa_cmd="numactl -a -i all -C 0-31"
-        ;;
-      16)
-        numa_cmd="numactl -a -i all -C 0-15"
-        ;;
-    esac
+    numa_node_param=0-$(( (thread / core_per_numa) - ( (thread % core_per_numa == 0) ? 1 : 0 ) ))
+    core_binding_param=0-$((thread - 1))
+    numa_cmd="numactl -a -i ${numa_node_param} -C ${core_binding_param}"
 
     if [[ "$dataset" == *"ungraph"* ]]; then
       OMP_PLACES=cores OMP_NUM_THREADS=$thread $numa_cmd $imm_bin -i "${test_data_dir}/$dataset" -u -w -p -k 50 -d LT -e 0.5 -o "$log_file"
